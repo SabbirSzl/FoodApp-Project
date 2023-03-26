de4708fdbb1fc4caf08d482a19784c5830b80dd1
Sabbir Ahmed Sozol <sabbir.sozol@northsouth.edu>
2023-03-27 00:27:09 +0600

    Added Not Found component to handle situations of emptiness.

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -7,24 +7,25 @@ import { Food } from 'src/app/shared/models/food';
 @Component({
   selector: 'app-food-page',
   templateUrl: './food-page.component.html',
-  styleUrls: ['./food-page.component.scss']
+  styleUrls: ['./food-page.component.scss'],
 })
-export class FoodPageComponent implements OnInit{
-food!:Food;
-  constructor(activatedRoute:ActivatedRoute,foodService:FoodService,
-    private cartService:CartService, private router:Router){
-    activatedRoute.params.subscribe((params)=> {
-      if(params.id)
-      {
+export class FoodPageComponent implements OnInit {
+  food!: Food;
+  constructor(
+    activatedRoute: ActivatedRoute,
+    foodService: FoodService,
+    private cartService: CartService,
+    private router: Router
+  ) {
+    activatedRoute.params.subscribe((params) => {
+      if (params.id) {
         this.food = foodService.getFoodById(params.id);
       }
-    })
+    });
   }
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
-  }
-
-  addToCart(){
+  addToCart() {
     this.cartService.addToCart(this.food);
     this.router.navigateByUrl('/cart-page');
   }
